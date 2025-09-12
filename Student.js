787cd5fb2a5ff07f84f543cabdf8df78c7fc44fa
Daniel John <danieljohnbyns@gmail.com>
2025-09-13 03:47:48 +0800

    Add files via upload

---
+++ Student.js
@@ -0,0 +1,61 @@
+/**
+ * @typedef {'BSCpE' | 'BSIT'} ICSPrograms
+ * @typedef {'BSEd-SCI' | 'BEEd-GEN' | 'BEEd-ECED' | 'BTLEd-ICT' | 'TCP'} ITEPrograms
+ * @typedef {'BSBA-HRM' | 'BSE'} IBEPrograms
+ */
+
+/**
+ * @typedef {{
+ * 	name: {
+ * 		first: String,
+ * 		middle: String,
+ * 		last: String
+ * 	},
+ * 	role: 'student' | 'unverified-student',
+ * 	email: String,
+ * 	phone: String,
+ * 	id: String,
+ * 	profilePicture?: String,
+ * 	status?: 'active' | 'restricted' | 'archived',
+ * 	organizations?: import('./Organization').OrganizationProps[],
+ * }} BaseStudentProps
+ */
+
+/** @typedef {BaseStudentProps & { institute: 'ics', program: ICSPrograms, year: Number }} ICSStudent */
+/** @typedef {BaseStudentProps & { institute: 'ite', program: ITEPrograms, year: Number }} ITEStudent */
+/** @typedef {BaseStudentProps & { institute: 'ibe', program: IBEPrograms, year: Number }} IBEStudent */
+
+/** @typedef {ICSStudent | ITEStudent | IBEStudent} StudentProps */
+
+class Student {
+	/**
+	 * @param {StudentProps} props
+	 */
+	constructor({
+		name,
+		role,
+		email,
+		phone,
+		id,
+		institute,
+		program,
+		year,
+		profilePicture = '/Placeholder Image.svg',
+		status = 'active',
+		organizations = []
+	}) {
+		this.name = name;
+		this.role = role;
+		this.email = email;
+		this.phone = phone;
+		this.id = id;
+		this.institute = institute;
+		this.program = program;
+		this.year = year;
+		this.profilePicture = profilePicture;
+		this.status = status;
+		this.organizations = organizations;
+	};
+};
+
+export default Student;
