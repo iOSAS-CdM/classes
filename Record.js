cad13398f748e50094ca6b091b5186ea492d48bf
Daniel John Baynosa <danieljohnbyns@gmail.com>
2025-09-23 14:51:00 +0800

    Update RecordProps typedef to specify progress as a union of numeric literals

--- Record.js
+++ Record.js
@@ -19,7 +19,7 @@
  * 	tags: {
  * 		status: RecordStatus,
  * 		severity: RecordSeverity,
- * 		progress: number
+ * 		progress: 0 | 1 | 2 | 3 | 4 | 5
  * 	},
  * 	date: Date,
  * 	repository?: string
